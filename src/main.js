ab60723f9f5ac91f1eddac749edcd1e3effeaaea
msj <1751871164@qq.com>
2020-02-12 22:10:23 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from "@/router/router";
+import "@/assets/styles/reset.css";
+import "@/assets/styles/border.css";
+import fastClick from "fastclick";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+fastClick.attach(document.body);
 
 new Vue({
+  router,
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
---
+++ src/router/router.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from "@/pages/home/Home";
+Vue.use(Router);
+
+
+const routes = [
+    {
+        path:"/",
+        name:'Home',
+        component:Home,
+    }
+];
+const router = new Router({
+    routes
+});
+
+export default router
