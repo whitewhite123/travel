9451554a38df7ec9c5f0178b0ec369b149c7d3b7
msj <1751871164@qq.com>
2020-02-13 10:17:38 +0800

    end index-swiper

--- src/main.js
+++ src/main.js
@@ -6,6 +6,11 @@ import "@/assets/styles/border.css";
 import "@/assets/styles/iconfont.css";
 import fastClick from "fastclick";
 
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
+
+Vue.use(VueAwesomeSwiper);
+
 Vue.config.productionTip = false;
 fastClick.attach(document.body);
 
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="wrapper">
+        <swiper :options="swiperOption">
+            <swiper-slide v-for="item of swiperList" :key="item.id">
+                <img class="swiper-img" :src="item.imgUrl"/>
+            </swiper-slide>
+            <div class="swiper-pagination"  slot="pagination"></div>
+        </swiper>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"HomeSwiper",
+        data(){
+            return{
+                swiperOption:{
+                    pagination:".swiper-pagination",
+                    loop:true,
+                },
+                swiperList:[
+                    {
+                        id:'001',
+                        imgUrl:"//imgs.qunarzz.com/vc/24/36/10/3fc94d042ae62de380581d4c98.jpg_92.jpg",
+                    },
+                    {
+                        id:"002",
+                        imgUrl: "//imgs.qunarzz.com/vc/ad/7c/fe/037d1a400372ff57b0030d1d1c.jpg_92.jpg",
+                    }
+                ]
+            }
+        },
+    }
+</script>
+
+<style lang="stylus" scoped>
+    .wrapper >>> .swiper-pagination-bullet-active{
+        background-color :white !important
+    }
+    .wrapper
+        overflow :hidden
+        width :100%
+        height :0
+        padding-bottom :31.35%
+        background-color :#ccc
+
+        .swiper-img{
+            width 100%
+        }
+</style>
+
