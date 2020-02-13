fa60a1a738c6c451ca4d177405d82e088d218c97
msj <1751871164@qq.com>
2020-02-13 20:23:02 +0800

    finish city-search

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="search">
+        <input class="search-input" type="text" placeholder="请输入城市名或者拼音">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "CitySearch",
+
+    }
+</script>
+
+<style lang="stylus" scoped>
+    @import "~@/assets/styles/varibles.styl"
+    .search
+        height :.72rem
+        background-color :$bgColor
+        padding:0 .1rem
+        .search-input
+            width :100%
+            height :.62rem
+            line-height :.62rem
+            text-align :center
+            border-radius :.06rem
+            color :#666
+            padding :0 .1rem
+            box-sizing :border-box
+
+</style>
